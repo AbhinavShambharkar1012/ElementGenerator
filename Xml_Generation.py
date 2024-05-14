4ff5062fa92b0b89aa3c9db8eeec1ca82fd9c1fc
AbhinavShambharkar1012 <165635832+AbhinavShambharkar1012@users.noreply.github.com>
2024-05-14 15:36:49 +0530

    Update Xml_Generation.py

--- Xml_Generation.py
+++ Xml_Generation.py
@@ -64,7 +64,7 @@ def get_file_name(mydata):
     )
 
     llm= ChatOpenAI(temperature=0,
-                model_name='gpt-3.5-turbo') 
+                model_name='gpt-3.5-turbo',api_key = "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") 
     query = "Return just File name under name tag with .xml extension"
     answer = llm.invoke(prompt.format(data1=mydata,query=query))
     return answer.content
