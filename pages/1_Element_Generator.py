0d9dd171401c0caf849ef57bab7b27d8afea05f1
AbhinavShambharkar1012 <165635832+AbhinavShambharkar1012@users.noreply.github.com>
2024-05-23 16:13:58 +0530

    Update 1_Element_Generator.py

--- pages/1_Element_Generator.py
+++ pages/1_Element_Generator.py
@@ -1,18 +1,28 @@
 import streamlit as st
 from Xml_Generation import myfunc,get_file_name
 from code_editor import code_editor
-#from streamlit_option_menu import option_menu
+from streamlit_option_menu import option_menu
 from class_generation import myfuncclass,get_class_name
+from RDP_generation import myfuncclassRDP
 import json
-
+import zipfile
+import os
+import base64
+from io import BytesIO
 
 st.set_page_config(layout="wide")
 
 if 'data' not in st.session_state:
     st.session_state['data'] = []
 
-if 'classes' not in st.session_state:
-    st.session_state['classes'] = []
+if 'class1' not in st.session_state:
+    st.session_state['class1'] = []
+
+if 'class2' not in st.session_state:
+    st.session_state['class2'] = []
+    
+if 'class3' not in st.session_state:
+    st.session_state['class3'] = []
 
 if 'User_Input' not in st.session_state:
     st.session_state['User_Input'] = []
@@ -50,10 +60,7 @@ st.markdown(
                 background-color : #FAFAFB;
                 
             }
-            .st-emotion-cache-1jicfl2{
-                padding-left : 1rem;
-                padding-right : 1rem;
-            }
+            
             [data-testid="stSidebarContent"]{
                 width:200px;
                 
@@ -146,7 +153,7 @@ st.markdown(
         </style>
         <body>
             <div class="Header">
-                <h2>GenX++ D365 Finance and Operation Element Generator ..</h2>
+                <h1>GenX++ D365 Finance and Operation Element Generator ..</h1>
             </div>
         </body>
         """,
@@ -182,7 +189,7 @@ with col1:
     
     option = st.selectbox(
     "",
-    ("Select element to create","Table", "Table Extension", "EDT","SysOperations Framework"))
+    ("Select element to create","Table", "Table Extension", "EDT","SysOperations Framework Batch Job","SSRS Report using DP class"))
     #object_to_create = st.radio( " ",('Table', 'Form', 'EDT'),horizontal=True)
 
     
@@ -192,9 +199,10 @@ with col1:
         option_data = {
             'Select element to create': 'Select element to create',
             'Table': "Generate F&O table Table name : 'Customer' , Fields : 'Name , Age , Place '",
-            'Table Extension': 'Generate Table extension for Table CustTable Model Name : AbhinavTestModel , Fields : MyExampleField',
+            'Table Extension': 'Create an extension of table familydata with field as myfield2',
             'EDT': 'Generate EDT with name TransfID',
-            'SysOperations Framework': 'Create sysoperation framework with naming convention AASBatchJob and parameters as transferId with EDT TransfID'
+            'SysOperations Framework Batch Job': 'Create sysoperation framework with naming convention AASBatchJob and parameters as transferId with EDT TransfID',
+            'SSRS Report using DP class' : 'Create  classes for  ssrs report using Report data provide class  with naming convention AASReportJob and parameters as CustomerID with EDT CustId to fetch data from CustTable.The fields in custTable are AccountNum, name ,  address and custGroup the temp table used is tmpCust'
         }
      
         def update_text_area(selected_option):
@@ -216,15 +224,74 @@ with col1:
             if option == "Select element to create":
                 st.error("Please Select element to create!")
             else:
-                if option == "SysOperations Framework":
+                if option == "SysOperations Framework Batch Job":
                     data = myfuncclass(st.session_state['User_Input'])
+                    file_name = "myfile.zip"
+                    
+                    if data:
+                        pass
+                    else:
+                        st.error("No data")
+                    output_dict = json.loads(data)
+                    classes = []
+                    class_names = []
+                    for key, value in output_dict.items():
+                        classes.append(value)
+                        class_names.append(key)
+
+                    st.session_state['class1'] = classes[0]
+                    st.session_state['class2'] = classes[1]
+                    st.session_state['class3'] = classes[2]
+                    #class_name = get_class_name(data)
+                    
+                    sub_container2 = st.container()
+                    with sub_container2:
+                        sub2_col1, sub2_col2 = st.columns([2,1])
+                        with sub2_col1:
+                            st.write(class_names)
+                    
+                    #st.write(output_dict)
+                    zip_data = BytesIO()
+                    with zipfile.ZipFile(zip_data, "w") as zipf:
+                        for filename, content in output_dict.items():
+                            # Write content to file in memory
+                            file_data = BytesIO()
+                            file_data.write(content.encode())
+                            file_data.seek(0)
+                            # Add file to zip
+                            zipf.writestr(filename, file_data.read())
+
+
+                    col_1 , col_2 = st.columns([1,2])
+                    final_data = zip_data.getvalue()
+                    #st.write(final_data)
+                    with col_1:
+                        st.download_button(
+                                    label="Download File",
+                                    data=final_data,
+                                    file_name=file_name,
+                                    mime='application/zip',
+                                )
+                    with col_2:
+                        clear = st.button(label="Clear")       
+                            
+                elif option == "SSRS Report using DP class":
+                    #/////rdp
+                    data = myfuncclassRDP(st.session_state['User_Input'])
                     file_name = "myfile.xpp"
                     
+                    if data:
+                        pass
+                    else:
+                        st.error("No data")
                     output_dict = json.loads(data)
                     classes = []
                     for key, value in output_dict.items():
                         classes.append(value)
-                    st.session_state['classes'] = classes[1]
+
+                    st.session_state['class1'] = classes[0]
+                    st.session_state['class2'] = classes[1]
+                    st.session_state['class3'] = classes[2]
                     class_name = get_class_name(data)
                     sub_container2 = st.container()
                     with sub_container2:
@@ -242,7 +309,6 @@ with col1:
                                 )
                     with col_2:
                         clear = st.button(label="Clear")       
-                            
 
                 else:
                     data = myfunc(st.session_state['User_Input'])
@@ -256,7 +322,7 @@ with col1:
                             st.image("image (2).png")
                         with sub2_col2:
                             st.write(file_name)
-                    col_1 , col_2 = st.columns([2,1])
+                    col_1 , col_2 = st.columns([1,2])
                     with col_1:
                         st.download_button(
                                     label="Download File",
@@ -269,7 +335,7 @@ with col1:
 
             
 with col2:
-    if option == "SysOperations Framework":
+    if option == "SysOperations Framework Batch Job" or option == "SSRS Report using DP class":
         Right_Container1 = col2.container()
         with Right_Container1:
             st.text("Code Preview")
@@ -286,12 +352,24 @@ with col2:
                 "commands": ["submit"],
                 "style": {"Top": "0.44rem", "right": "0.4rem"}
             }]
-            mycode1 = st.session_state['classes']
-            
+            mycode1 = st.session_state['class1']
+            mycode2 = st.session_state['class2']
+            mycode3 = st.session_state['class3']
+
             if mycode1:
                 mycode1 = mycode1
             else:
                 mycode1 = "Your Output class will be shown here"
+
+            if mycode2:
+                mycode2 = mycode2
+            else:
+                mycode2 = "Your Output class will be shown here"
+
+            if mycode3:
+                mycode3 = mycode3
+            else:
+                mycode3 = "Your Output class will be shown here"
 
             mytheme = st.session_state['theme']
             
@@ -300,11 +378,19 @@ with col2:
             else:
                 mytheme = "Default"
 
+
+            if 'secondeditor' in st.session_state:
+                st.session_state.secondeditor = mycode2
+
+            if 'thirdeditor' in st.session_state:
+                st.session_state.thirdeditor = mycode3 
+                
+
             code_editor1 = code_editor(mycode1,lang="xml",theme=mytheme,info="code preview",buttons=editor_btns,options={"wrap": True, "showLineNumbers": True})
 
-            code_editor2 = code_editor(mycode1,lang="xml",theme=mytheme,info="code preview",key="secondeditor",buttons=editor_btns,options={"wrap": True, "showLineNumbers": True})
+            code_editor2 = code_editor(mycode2,lang="xml",theme=mytheme,info="code preview",buttons=editor_btns,options={"wrap": True, "showLineNumbers": True})
 
-            code_editor3 = code_editor(mycode1,lang="xml",theme=mytheme,info="code preview",key="thirdeditor",buttons=editor_btns,options={"wrap": True, "showLineNumbers": True})
+            code_editor3 = code_editor(mycode3,lang="xml",theme=mytheme,info="code preview",buttons=editor_btns,options={"wrap": True, "showLineNumbers": True})
 
             
 
@@ -313,7 +399,7 @@ with col2:
                         st.session_state['theme'] = "Default"
                 else:
                         st.session_state['theme'] = "dark"
-        st.write(st.session_state)
+        #st.write(st.session_state)
     else:
         Right_Container = col2.container()
         with Right_Container:
